ad59b7805b892f897ff804a9f657a4e1b44c7497
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2021-01-11 20:59:25 -0300

    Continuacao: Funcao (Construtoras)

---
+++ funcao/funcaoConstrutora.js
@@ -0,0 +1,35 @@
+function Carro(velocidadeMaxima = 200, delta = 5){
+    // atributo privado
+    let velocidadeAtual = 0
+
+    // metodo publico
+    this.acelerar = function () {
+        if (velocidadeAtual + delta <= velocidadeMaxima) {
+            velocidadeAtual += delta
+        } else{
+            velocidadeAtual = velocidadeMaxima
+        }
+    }
+
+    // metodo publico
+    this.getVelocidadeAtual = function () {
+        return velocidadeAtual
+    }
+}
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+console.log("------------------------")
+
+const ferrari = new Carro(350, 20)
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
+console.log("------------------------")
+
+const maverick = new Carro(250, 50)
+maverick.acelerar()
+maverick.acelerar()
+maverick.acelerar()
+console.log(maverick.getVelocidadeAtual())
+console.log("------------------------")
