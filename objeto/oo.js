06738fc04a0c868a0b800148faf68764ef460f79
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2021-01-21 19:33:35 -0300

    Inicio: Introdução á OO

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural 
+processamento( valor1, valor2, valor3)
+
+// Orientação Objeto OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() // O foco passou a ser objeto
+
+// Principios básicos
+// 1. abstração
+// 2. encapsulamento
+// 3. herança  ---- (prototype)
+// 4. polimorfismo
